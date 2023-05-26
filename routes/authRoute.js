390615b4e14233146c4dc1bb4ab90095631402cc
Rutvik Makvana <rutvikmakvana432@gmail.com>
2023-05-26 15:04:21 +0530

    feat : create forgot and reset password routes

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -10,13 +10,18 @@ const {
   updateUser,
   blockedUser,
   unblockedUser,
-  updatePassword
+  updatePassword,
+  forgotPasswordToken,
+  resetPassword
 } = require("../controller/userController");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/register", createUser);
+router.post("/forgot-password-token", forgotPasswordToken);
+router.put("/reset-password/:token", resetPassword);
 router.put('/update-password', authMiddleware, updatePassword)
+
 router.post("/login", loginUserCtrl);
 router.get('/refresh', handleRefreshToken);
 router.get('/logout', logout)
