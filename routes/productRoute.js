b6d66258c0ad95050f1cff5c5c4d231d2b851600
Rutvik Makvana <rutvikmakvana432@gmail.com>
2023-05-30 14:27:24 +0530

    feat : create rating functionality for product

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const { createProduct , getProduct, getAllProducts,updateProduct, deleteProduct, addToWishlist} = require("../controller/productController");
+const { createProduct , getProduct, getAllProducts,updateProduct, deleteProduct, addToWishlist, rating} = require("../controller/productController");
 const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/create-product", authMiddleware, isAdmin, createProduct);
 router.get("/get-product/:id", getProduct);
+
 router.put("/wishlist", authMiddleware, addToWishlist);
+router.put("/rating", authMiddleware, rating);
+
 router.get("/list", getAllProducts);
 router.put("/update-product/:id", authMiddleware, isAdmin, updateProduct)
 router.delete("/delete-product/:id", authMiddleware, isAdmin, deleteProduct)
